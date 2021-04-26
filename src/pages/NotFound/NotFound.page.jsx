6522b6379bee3c5cca3cd3604772100107538a83
Gerardo Cabrera <gerardojaviercr@gmail.com>
2021-04-26 10:43:48 -0500

    Modified Not Found page

--- src/pages/NotFound/NotFound.page.jsx
+++ src/pages/NotFound/NotFound.page.jsx
@@ -5,11 +5,14 @@ import './NotFound.styles.css';
 
 function NotFoundPage() {
   return (
-    <section className="not-found">
-      <Link to="/" className="home-link">
-        home
-      </Link>
+    <section className="not-found" style={{ paddingTop: '80px', textAlign: 'center' }}>
       <img src="404.gif" alt="page not found" />
+      <h1 style={{ paddingTop: '30px' }}>
+        This is not the page you are looking for ...
+        <Link to="/" className="home-link">
+          Move along (Go Home)
+        </Link>
+      </h1>
     </section>
   );
 }
